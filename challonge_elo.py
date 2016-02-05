943143c2f5ba3e485fcd30d7e01879de9baed3bf
Max Timkovich <mtimkovich@gmail.com>
2016-02-04 22:15:54 -0800

    Elo calculated for last 5 tournaments

--- challonge_elo.py
+++ challonge_elo.py
@@ -1,21 +1,42 @@
 #!/usr/bin/env python
 import challonge
 import config
+import pprint
+import trueskill
+
+# pp = pprint.PrettyPrinter()
 
 challonge.set_credentials(config.user, config.api_key)
-tournament_id = config.subdomain+'-SDHS27'
 
-matches = challonge.matches.index(tournament_id)
-players = challonge.participants.index(tournament_id)
+players = {}
 
-tag = {player['id']: player['name'] for player in players}
+tournaments = ['SDHS27', 'SDHS26', 'SDHS25', 'SDHS24', 'SDHS']
 
-for match in matches:
-    winner = tag[match['winner-id']]
-    player1 = tag[match['player1-id']]
-    player2 = tag[match['player2-id']]
+for tournament in tournaments:
+    tournament_id = config.subdomain + '-' + tournament
 
-    if winner == player1:
-        print '{} beat {}'.format(player1, player2)
-    else:
-        print '{} beat {}'.format(player2, player1)
+    matches = challonge.matches.index(tournament_id)
+    participants = challonge.participants.index(tournament_id)
+
+    tag = {}
+
+    for p in participants:
+        name = p['name']
+        tag[p['id']] = name
+
+        if name not in players:
+            players[name] = trueskill.Rating()
+
+
+    for match in matches:
+        winner = tag[match['winner-id']]
+        one = tag[match['player1-id']]
+        two = tag[match['player2-id']]
+
+        if winner == one:
+            players[one], players[two] = trueskill.rate_1vs1(players[one], players[two])
+        else:
+            players[two], players[one] = trueskill.rate_1vs1(players[two], players[one])
+
+for player in sorted(players, key=players.get, reverse=True):
+    print '{}: {}'.format(player, players[player].mu)
