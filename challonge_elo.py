e996fc00bab220804a5b34f611a6343a2be477dc
Max Timkovich <mtimkovich@gmail.com>
2016-02-04 21:43:52 -0800

    Get all the matches and all tags

--- challonge_elo.py
+++ challonge_elo.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 import challonge
+import config
 
+challonge.set_credentials(config.user, config.api_key)
+tournament_id = config.subdomain+'-SDHS27'
+
+matches = challonge.matches.index(tournament_id)
+players = challonge.participants.index(tournament_id)
+
+tag = {player['id']: player['name'] for player in players}
+
+for match in matches:
+    winner = tag[match['winner-id']]
+    player1 = tag[match['player1-id']]
+    player2 = tag[match['player2-id']]
+
+    if winner == player1:
+        print '{} beat {}'.format(player1, player2)
+    else:
+        print '{} beat {}'.format(player2, player1)
